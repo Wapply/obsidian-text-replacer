35462e46eefd75ce70c119bea89638feeabc1ca5
Wapply <phoenixlord9707@gmail.com>
2024-03-29 20:06:11 -0300

    1.0.0

--- main.ts
+++ main.ts
@@ -1,13 +1,24 @@
-import { Plugin, MarkdownView, TFile } from 'obsidian';
+import { Plugin, MarkdownView } from 'obsidian';
+import { TextReplacerSettingsTab } from './settings'; // Adjust the path to match your directory structure
 
-interface TextReplacerSettings {
+interface Rule {
     startDelimiter: string;
     endDelimiter: string;
+    replacementText: string;
+}
+
+interface TextReplacerSettings {
+    rules: Rule[];
 }
 
 const DEFAULT_SETTINGS: TextReplacerSettings = {
-    startDelimiter: '{{',
-    endDelimiter: '}}'
+    rules: [
+        {
+            startDelimiter: 'FIRST_DELIMITER',
+            endDelimiter: 'SECOND_DELIMITER',
+            replacementText: 'REPLACED',
+        }
+    ]
 };
 
 export default class TextReplacerPlugin extends Plugin {
@@ -16,35 +27,41 @@ export default class TextReplacerPlugin extends Plugin {
     async onload() {
         await this.loadSettings();
 
+        // Add the command for replacing text between delimiters
         this.addCommand({
             id: 'replace-text-between',
             name: 'Replace Text Between Delimiters',
             callback: () => this.replaceTextBetween(),
         });
 
+        // Add the settings tab
         this.addSettingTab(new TextReplacerSettingsTab(this.app, this));
+
+        // Add the icon button to the ribbon
+        this.addRibbonIcon('replace', 'Replace Text', () => {
+            this.replaceTextBetween();
+        });
     }
 
     async replaceTextBetween() {
         const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
         if (activeView) {
             const editor = activeView.editor;
-            const {startDelimiter, endDelimiter} = this.settings;
+            const content = editor.getValue();
 
-            const selectedText = editor.getSelection();
+            let newText = content;
 
-            if (selectedText) {
-                const newText = selectedText.replace(
+            this.settings.rules.forEach(rule => {
+                const { startDelimiter, endDelimiter, replacementText } = rule;
+                newText = newText.replace(
                     new RegExp(`${startDelimiter}([\\s\\S]*?)${endDelimiter}`, 'g'),
                     (match, content) => {
-                        // Here you can implement the logic to replace the content
-                        // For simplicity, let's just return the content as it is
-                        return content;
+                        return `${startDelimiter}${replacementText}${endDelimiter}`;
                     }
                 );
+            });
 
-                await editor.replaceSelection(newText);
-            }
+            await editor.setValue(newText);
         }
     }
 
--- settings.ts
+++ settings.ts
@@ -1,7 +1,8 @@
-import { PluginSettingTab, App, Setting } from 'obsidian';
+import { PluginSettingTab, App, Setting, ButtonComponent } from 'obsidian';
 
 export class TextReplacerSettingsTab extends PluginSettingTab {
     plugin: any;
+    rulesContainer: HTMLElement;
 
     constructor(app: App, plugin: any) {
         super(app, plugin);
@@ -13,28 +14,71 @@ export class TextReplacerSettingsTab extends PluginSettingTab {
 
         containerEl.empty();
 
-        containerEl.createEl('h2', {text: 'Text Replacer Settings'});
+        this.rulesContainer = containerEl.createEl('div', { attr: { class: 'text-replacer-rules' } });
 
-        new Setting(containerEl)
-            .setName('Start Delimiter')
-            .setDesc('Enter the start delimiter')
-            .addText(text => text
-                .setPlaceholder('e.g., {{')
-                .setValue(this.plugin.settings.startDelimiter)
-                .onChange(async (value) => {
-                    this.plugin.settings.startDelimiter = value;
-                    await this.plugin.saveSettings();
-                }));
+        this.renderRules();
+    }
 
-        new Setting(containerEl)
-            .setName('End Delimiter')
-            .setDesc('Enter the end delimiter')
-            .addText(text => text
-                .setPlaceholder('e.g., }}')
-                .setValue(this.plugin.settings.endDelimiter)
-                .onChange(async (value) => {
-                    this.plugin.settings.endDelimiter = value;
-                    await this.plugin.saveSettings();
-                }));
+    renderRules() {
+        this.rulesContainer.empty();
+
+        for (let i = 0; i < this.plugin.settings.rules.length; i++) {
+            const rule = this.plugin.settings.rules[i];
+
+            const ruleDiv = this.rulesContainer.createEl('div', { attr: { class: 'text-replacer-rule' } });
+
+            new Setting(ruleDiv)
+                .setName('Start Delimiter')
+                .addText(text => text
+                    .setValue(rule.startDelimiter)
+                    .onChange(async (value) => {
+                        this.plugin.settings.rules[i].startDelimiter = value;
+                        await this.plugin.saveSettings();
+                    }));
+
+            new Setting(ruleDiv)
+                .setName('End Delimiter')
+                .addText(text => text
+                    .setValue(rule.endDelimiter)
+                    .onChange(async (value) => {
+                        this.plugin.settings.rules[i].endDelimiter = value;
+                        await this.plugin.saveSettings();
+                    }));
+
+            new Setting(ruleDiv)
+                .setName('Replacement Text')
+                .addText(text => text
+                    .setValue(rule.replacementText)
+                    .onChange(async (value) => {
+                        this.plugin.settings.rules[i].replacementText = value;
+                        await this.plugin.saveSettings();
+                    }));
+
+            new Setting(ruleDiv)
+                .setName('Remove Rule')
+                .addButton(button => button
+                    .setButtonText('Remove')
+                    .onClick(async () => {
+                        this.plugin.settings.rules.splice(i, 1);
+                        await this.plugin.saveSettings();
+                        this.renderRules(); // Refresh only the rules section
+                    }));
+        }
+
+        // Add the "Add Rule" button at the end of the rules
+        new Setting(this.rulesContainer)
+            .setName('Add Rule')
+            .addButton(button => button
+                .setButtonText('Add')
+                .onClick(() => this.addRule()));
+    }
+
+    addRule() {
+        this.plugin.settings.rules.push({
+            startDelimiter: '',
+            endDelimiter: '',
+            replacementText: ''
+        });
+        this.renderRules(); // Refresh only the rules section
     }
 }
